76e5540da1ed96c82f6a802459ed9ab82d5ae5f1
Sagar Sen <sagarsen.dev@gmail.com>
2024-08-15 15:19:47 -0700

    Implemented proper type safe components

--- food-ordering-app/src/app/(tabs)/index.tsx
+++ food-ordering-app/src/app/(tabs)/index.tsx
@@ -1,35 +1,17 @@
-import { StyleSheet, Image } from 'react-native';
-import products from '@/assets/data/products';
-import { Text, View } from '@/src/components/Themed';
+import { StyleSheet, Image } from "react-native";
+import products from "@/assets/data/products";
+import { Text, View } from "@/src/components/Themed";
+import ProductCard from "@/src/components/ProductCard";
+
 
 export default function TabOneScreen() {
-  const singleProduct = products[0]
   return (
-    <View style={styles.container}>
-      <Image source={{uri: singleProduct.image}} style={styles.image}/>
-     <Text style={styles.title}>{singleProduct.name}</Text>
-     <Text style={styles.description}>${singleProduct.price}</Text>
+    <View>
+      {products.map((item, index) => (
+        <ProductCard singleProduct={item} key={index} />
+      ))}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-   backgroundColor: "#808080",
-   margin: 10,
-   padding: 10,
-   borderRadius: 10,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  description: {
-    marginTop: 10,
-    fontSize: 16,
-  },
-});
+
---
+++ food-ordering-app/src/components/ProductCard.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Image } from "react-native";
+import { Text, View } from "@/src/components/Themed";
+import { Product } from "@/src/types";
+
+type ProductcardProps = {
+  singleProduct: Product;
+};
+
+const ProductCard = ({ singleProduct }: ProductcardProps) => (
+  <View style={styles.container}>
+    <Image
+      source={{
+        uri:
+          singleProduct.image ||
+          "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png",
+      }}
+      style={styles.image}
+    />
+    <Text style={styles.title}>{singleProduct.name}</Text>
+    <Text style={styles.description}>${singleProduct.price}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#808080",
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  description: {
+    marginTop: 10,
+    fontSize: 16,
+  },
+});
+
+export default ProductCard;
