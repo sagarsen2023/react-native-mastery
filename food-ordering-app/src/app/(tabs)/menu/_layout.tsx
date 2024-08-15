e67848e4e9b7e5b84d980b271222803639f2acc4
Sagar Sen <sagarsen.dev@gmail.com>
2024-08-15 20:41:30 +0530

    Delete food-ordering-app directory

--- food-ordering-app/src/app/(tabs)/menu/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function Menu () {
-    return (
-       <Stack>
-        <Stack.Screen name="index"  options={{title: "Menu"}}/>
-       </Stack>
-    )
-}
