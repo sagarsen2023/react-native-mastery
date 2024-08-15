6e9c1a80cdc8fcb4ee791f0a76f086564d855094
Sagar Sen <sagarsen.dev@gmail.com>
2024-08-15 14:22:34 -0700

    Styled our first page using native stylesheet utility

--- food-ordering-app/src/app/(tabs)/index.tsx
+++ food-ordering-app/src/app/(tabs)/index.tsx
@@ -1,31 +1,35 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { StyleSheet, Image } from 'react-native';
+import products from '@/assets/data/products';
 import { Text, View } from '@/src/components/Themed';
 
 export default function TabOneScreen() {
+  const singleProduct = products[0]
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{uri: singleProduct.image}} style={styles.image}/>
+     <Text style={styles.title}>{singleProduct.name}</Text>
+     <Text style={styles.description}>${singleProduct.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+   backgroundColor: "#808080",
+   margin: 10,
+   padding: 10,
+   borderRadius: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  description: {
+    marginTop: 10,
+    fontSize: 16,
   },
 });
