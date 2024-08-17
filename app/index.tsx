33203b8233cd54f2dc69c7bf430b6d21e41b0e8e
Sagar Sen <sagarsen.dev@gmail.com>
2024-08-17 19:10:23 +0530

    Cleaned the project structure and added assets with splash screens configuration

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const index = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text className='text`-2xl'>Uber Clone</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default index
