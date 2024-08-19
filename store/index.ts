206557d55a6c918c66f077692de45539b34afabb
Sagar Sen <sagarsen.dev@gmail.com>
2024-08-19 12:59:56 +0530

    Initialized zustand for global state management

---
+++ store/index.ts
@@ -0,0 +1,41 @@
+import { LocationStore } from "@/types/type";
+import { create } from "zustand";
+
+export const useLocationStore = create<LocationStore>((set) => ({
+  userAddress: null,
+  userLatitude: null,
+  userLongitude: null,
+  destinationAddress: null,
+  destinationLatitude: null,
+  destinationLongitude: null,
+  setUserLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      userAddress: address,
+      userLatitude: latitude,
+      userLongitude: longitude,
+    }));
+  },
+  setDestinationLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      destinationAddress: address,
+      destinationLatitude: latitude,
+      destinationLongitude: longitude,
+    }));
+  },
+}));
